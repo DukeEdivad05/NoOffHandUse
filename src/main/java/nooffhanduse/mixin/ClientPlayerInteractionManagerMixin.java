925e1ac4aa5223ea36f5b7446296a5aea5cb3a2b
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2022-07-07 10:53:53 +0200

    BaseMod(NoConfig)

--- src/main/java/nooffhanduse/mixin/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/nooffhanduse/mixin/ClientPlayerInteractionManagerMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -17,17 +18,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ClientPlayerInteractionManagerMixin {
     @Inject(method = "interactBlock", at = @At("HEAD"), cancellable = true)
     private void interactBlockMixin(ClientPlayerEntity player, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
-        cir.setReturnValue(ActionResult.FAIL);
+        boolean shield = player.getOffHandStack().getItem() == Items.SHIELD;
+        boolean food = player.getOffHandStack().isFood();
+        boolean rocket = player.getOffHandStack().getItem() == Items.FIREWORK_ROCKET;
+        if(!shield && !food && !rocket) {
+            cir.setReturnValue(ActionResult.FAIL);
+        }
     }
 
     @Inject(method = "interactBlockInternal", at = @At("HEAD"), cancellable = true)
     private void interactBlockInternalMixin(ClientPlayerEntity player, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
-        cir.setReturnValue(ActionResult.FAIL);
+        boolean shield = player.getOffHandStack().getItem() == Items.SHIELD;
+        boolean food = player.getOffHandStack().isFood();
+        boolean rocket = player.getOffHandStack().getItem() == Items.FIREWORK_ROCKET;
+        if(!shield && !food && !rocket) {
+            cir.setReturnValue(ActionResult.FAIL);
+        }
     }
 
     @Inject(method = "interactItem", at = @At("HEAD"), cancellable = true)
     private void interactItemMixin(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-        cir.setReturnValue(ActionResult.FAIL);
+        boolean shield = player.getOffHandStack().getItem() == Items.SHIELD;
+        boolean food = player.getOffHandStack().isFood();
+        boolean rocket = player.getOffHandStack().getItem() == Items.FIREWORK_ROCKET;
+        if(!shield && !food && !rocket) {
+            cir.setReturnValue(ActionResult.FAIL);
+        }
     }
 
     @Inject(method = "interactEntity", at = @At("HEAD"), cancellable = true)
